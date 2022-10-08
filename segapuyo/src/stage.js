c275e028dece54794606d1b6ea3b0e9adf4ec497
iHdkz <i.hidekazu@gmail.com>
2022-10-08 22:10:05 +0900

    fix: stage.js

--- segapuyo/src/stage.js
+++ segapuyo/src/stage.js
@@ -8,7 +8,7 @@ class Stage {
     // static eraseStartFrame;
     // static erasingPuyoInfoList = [];
 
-    static initilaize() {
+    static initialize() {
         // HTML からステージの元となる要素を取得し、大きさを設定する
         const stageElement = document.getElementById("stage");
         stageElement.style.width = Config.puyoImgWidth * Config.stageCols + 'px';
